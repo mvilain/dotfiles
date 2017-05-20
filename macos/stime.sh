e98081d8226f1bd43c7809d9c90b5955620d00a5
Michael Vilain <michael@vilain.com>
2017-05-20 13:17:32 -0700

    add MacOS scripts

---
+++ macos/stime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# speak the time every 5 seconds until control-c
+#
+# http://hints.macworld.com/article.php?story=2011043006554823
+#
+echo "saying the time...press ^C to exit"
+while [ 1 ]; do
+	z=`date +%S`
+	if [ `expr $z % 5` -eq 0 ]; then 
+		say `date "+%l %M and %S seconds"`
+	fi
+done
+
