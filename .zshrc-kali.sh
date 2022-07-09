ae8e11972a2543014e08af26d91b203e33f9e590
Michael Vilain <michael@vilain.com>
2022-07-09 09:30:49 -0700

    comment out auto-suggestions

--- .zshrc-kali.sh
+++ .zshrc-kali.sh
@@ -225,7 +225,7 @@ done
 
 # enable auto-suggestions based on the history
 if [ -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-    . /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    # . /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     # change suggestion color
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
 fi
