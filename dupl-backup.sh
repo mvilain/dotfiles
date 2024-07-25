e96cc63fdaa9f09a384c3b7ab1bc1998ed09822a
Michael Vilain <michael@vilain.com>
2024-07-25 08:03:59 -0700

    change signing key; cleanup older backup and report

--- dupl-backup.sh
+++ dupl-backup.sh
@@ -10,13 +10,14 @@ CONF=~/.duplicity/.env_variables.conf
 EXCL_FILE=exclude-${SCRIPT}
 DUR=7D
 TMPDIR=/mnt/backups/tmp/
+LOG=/var/log/${SCRIPT}-${NAME}.log
 DEST=file:///mnt/backups/zorin/
 
 [ ! -e ${TMPDIR} ] && echo "${TMPDIR} and ${DEST} not found" && exit 1
 
 # these should be in ~/.duplicity/.env-variables
-GPG_KEY=AF387140DA632FC0D7D97E8FA73B71C8020FF318
-ENC_KEY="020ff318" # zorin-backup
+GPG_KEY=473CE91EAC5BEBC3459A429BD26BD6CBD7CA8525
+ENC_KEY=D7CA8525
 [ -e ${CONF} ] && source ${CONF}
 [ -z "$GPG_KEY" ] && echo "GPG_KEY not defined" && exit 1
 # PASSPHRASE can be underdefined
@@ -29,14 +30,31 @@ cat >${EXCL_FILE} <<-EOF
 EOF
 
 date
+
+duplicity \
+  --encrypt-sign-key=${ENC_KEY} \
+  remove-older-than 90D --force ${DEST}
+
 duplicity backup --verbosity Error \
   --encrypt-sign-key=${GPG_KEY} \
   --tempdir ${TMPDIR} \
-  --log-file ~/.duplicity/info-${NAME}.log \
+  --log-file ${LOG} \
   --full-if-older-than ${DUR} \
   --exclude-other-filesystems --exclude-device-files \
   --exclude-filelist ${EXCL_FILE} \
   / ${DEST}
+
+duplicity \
+  cleanup --force \
+  --encrypt-sign-key=${ENC_KEY} \
+  ${DEST}
+
+duplicity collection-status \
+  --encrypt-sign-key=${ENC_KEY} \
+  ${DEST}
+
 date
+
+cat ${LOG}
 
 exit
